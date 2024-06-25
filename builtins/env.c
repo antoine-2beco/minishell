35b73d791bdb3501ec678fec22810a5d8ccbaae9
antoine-2beco <mail@antoinedebeco.com>
2024-06-25 15:12:09 +0200

    fixing envcmd

--- builtins/env.c
+++ builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hle-roi <hle-roi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 14:45:59 by ade-beco          #+#    #+#             */
-/*   Updated: 2024/06/17 13:50:23 by hle-roi          ###   ########.fr       */
+/*   Updated: 2024/06/25 15:10:58 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int	envcmd(char **args, t_data *data)
 	i = 0;
 	while (data->env[i])
 	{
-		ft_printf("%s\n", 1, data->env[i++]);
+		if (ft_strchr(data->env[i], '='))
+			ft_printf("%s\n", 1, data->env[i]);
+		i++;
 	}
 	return (1);
 }
