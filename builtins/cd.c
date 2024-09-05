eee9a9bfca63082f644ba7e9dd43a20c5071bbd5
Hugo Le roi <83402857+Tayra46@users.noreply.github.com>
2024-09-05 11:43:49 +0200

    change cd way of working

--- builtins/cd.c
+++ builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
+/*   By: hle-roi <hle-roi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:07:40 by hle-roi           #+#    #+#             */
-/*   Updated: 2024/09/03 14:55:06 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/09/03 15:35:15 by hle-roi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,12 @@
 
 int	change_cwd(char *folder, t_data *data)
 {
-	char	*new_path;
-	char	*temp;
-	char	path[100];
-
-	getcwd(path, sizeof(path));
-	if (path[0])
-	{
-		temp = ft_strjoin(path, "/");
-		new_path = ft_strjoin(temp, folder);
-		free(temp);
-	}
-	else
-		new_path = ft_strdup(folder);
-	if (chdir(new_path) == -1)
+	if (chdir(folder) == -1)
 	{
 		data->exitcode = 1;
 		perror("minishell");
 		return (1);
 	}
-	free(new_path);
 	data->exitcode = 0;
 	return (1);
 }
