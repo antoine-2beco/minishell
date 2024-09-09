fee92a30420df58ff6dabbe7696aade2e7d1a482
antoine-2beco <mail@antoinedebeco.com>
2024-09-09 10:27:37 +0200

    echo normed

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,30 +6,16 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:48:21 by ade-beco          #+#    #+#             */
-/*   Updated: 2024/09/03 12:48:26 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/09/09 10:26:33 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minilib.h"
 
-int	echocmd(char **args, t_data *data)
+static void	print_chars(char **args, int i, int n_flag)
 {
-	int		i;
-	int		j;
-	int		n_flag;
+	int	j;
 
-	i = 1;
-	n_flag = 0;
-	if (!args[1])
-	{
-		write(1, "\n", 1);
-		data->exitcode = 0;
-		return (1);
-	}
-	if (args[1][0] && args[1][1] && args[1][0] == '-' && args[1][1] != 'n')
-		return (0);
-	while (args[i] && ft_strcmp(args[i], "-n") == 0)
-		n_flag = i++;
 	while (args[i])
 	{
 		j = 0;
@@ -44,6 +30,26 @@ int	echocmd(char **args, t_data *data)
 			write(1, " ", 1);
 		i++;
 	}
+}
+
+int	echocmd(char **args, t_data *data)
+{
+	int		i;
+	int		n_flag;
+
+	i = 1;
+	n_flag = 0;
+	if (!args[1])
+	{
+		write(1, "\n", 1);
+		data->exitcode = 0;
+		return (1);
+	}
+	if (args[1][0] && args[1][1] && args[1][0] == '-' && args[1][1] != 'n')
+		return (0);
+	while (args[i] && ft_strcmp(args[i], "-n") == 0)
+		n_flag = i++;
+	print_chars(args, i, n_flag);
 	if (!n_flag)
 		write(1, "\n", 1);
 	data->exitcode = 0;
