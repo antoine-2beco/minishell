57f56622a7eeedb89b61cf12d76d00f6a9fd4e24
antoine-2beco <mail@antoinedebeco.com>
2024-09-18 16:53:04 +0200

    cd fixed norm and leaks

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:07:40 by hle-roi           #+#    #+#             */
-/*   Updated: 2024/09/18 16:38:34 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/09/18 16:51:03 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,7 @@ static int	update_oldpwd(t_list *env, int len)
 static int	set_directory(char *arg, t_list *env, t_data *data)
 {
 	if (!update_oldpwd(env, 7))
-	{
-		free(arg);
-		return (1);
-	}
+		return (free(arg), 1);
 	if (chdir(arg) == -1)
 	{
 		free (arg);
@@ -67,19 +64,13 @@ static int	handle_s_dash(char *arg, t_list *env, t_data *data)
 		tmp = get_env_var(data->env, "OLDPWD");
 		free(arg);
 		if (!tmp)
-		{
-			perror("minishell");
-			return (1);
-		}
+			return (perror("minishell"), 1);
 		arg = ft_strdup(tmp);
 		free(tmp);
 		set_directory(arg, env, data);
 		tmp = get_env_var(data->env, "PWD");
 		if (!tmp)
-		{
-			perror("minishell");
-			return (1);
-		}
+			return (perror("minishell"), 1);
 		ft_printf("%s\n", 1, arg);
 		free(tmp);
 		return (1);
@@ -89,31 +80,27 @@ static int	handle_s_dash(char *arg, t_list *env, t_data *data)
 
 static int	handle_tilde(t_data *data, t_list *env, char *arg)
 {
-	char	*tmp;
-	char	*tmp2;
+	char	*tmp[2];
 
 	if (!ft_strcmp(arg, "~") || !ft_strcmp(arg, "--") \
 		|| !ft_strncmp(arg, "~/", 2))
 	{
-		tmp = get_env_var(data->env, "HOME");
-		if (!tmp)
-		{
-			perror("minishell");
-			return (1);
-		}
+		tmp[0] = get_env_var(data->env, "HOME");
+		if (!tmp[0])
+			return (perror("minishell"), 1);
 		if (!ft_strncmp(arg, "~/", 2))
 		{
-			tmp2 = ft_substr(arg, 1, ft_strlen(arg));
+			tmp[1] = ft_substr(arg, 1, ft_strlen(arg));
 			free (arg);
-			arg = ft_strjoin(tmp, tmp2);
-			free(tmp2);
+			arg = ft_strjoin(tmp[0], tmp[1]);
+			free(tmp[1]);
 		}
 		else
 		{
-			free (arg);
-			arg = ft_strdup(tmp);
+			free(arg);
+			arg = ft_strdup(tmp[0]);
 		}
-		free(tmp);
+		free(tmp[0]);
 	}
 	return (handle_s_dash(arg, env, data));
 }
